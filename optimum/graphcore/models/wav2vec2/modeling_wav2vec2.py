35bb0d96cc2b6a982bc2502bfc2546c55d9386fe
Harry Mellor <harrym@graphcore.ai>
2022-02-23 12:05:05 +0000

    Add blank pipelined model

---
+++ optimum/graphcore/models/wav2vec2/modeling_wav2vec2.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+
+import poptorch
+from optimum.utils import logging
+from transformers import (
+    Wav2Vec2ForPreTraining,
+)
+
+from ...modeling_utils import PipelineMixin, register
+
+
+logger = logging.get_logger(__name__)
+
+
+@register(Wav2Vec2ForPreTraining)
+class PipelinedWav2Vec2ForPreTraining(Wav2Vec2ForPreTraining, PipelineMixin):
+    def __init__(self, *args, **kwargs) -> None:
+        logger.info("In subclass")
+        super().__init__(*args, **kwargs)
+
+
