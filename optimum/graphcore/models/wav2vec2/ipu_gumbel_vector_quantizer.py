57a3afc19aa4ce3d0379f33c63b3d792d4f7002a
Manuele Sigona <manueles@graphcore.ai>
2022-03-14 15:44:56 +0000

    Use a matmul in quantizer instead of index_select, in order to be able to backpropagate through the indices as well

--- optimum/graphcore/models/wav2vec2/ipu_gumbel_vector_quantizer.py
+++ optimum/graphcore/models/wav2vec2/ipu_gumbel_vector_quantizer.py
@@ -52,8 +52,6 @@ class IPUWav2Vec2GumbelVectorQuantizer(Wav2Vec2GumbelVectorQuantizer):
             codevector_probs = _ipu_gumbel_softmax(
                 hidden_states.float(), tau=self.temperature, hard=True
             ).type_as(hidden_states)
-            # Get the indices corresponding to entries with value 1
-            index = codevector_probs.argmax(dim=-1)
 
             # compute perplexity
             codevector_soft_dist = torch.softmax(
@@ -64,7 +62,6 @@ class IPUWav2Vec2GumbelVectorQuantizer(Wav2Vec2GumbelVectorQuantizer):
             # take argmax in non-differentiable way
             # comptute hard codevector distribution (one hot)
             codevector_idx = hidden_states.argmax(dim=-1)
-            index = codevector_idx
 
             update_values = torch.ones_like(codevector_idx.view(-1, 1), dtype=hidden_states.dtype)
             
@@ -75,12 +72,11 @@ class IPUWav2Vec2GumbelVectorQuantizer(Wav2Vec2GumbelVectorQuantizer):
 
             perplexity = self._compute_perplexity(codevector_probs, mask_time_indices)
 
-        index = index.view(batch_size * sequence_length, self.num_groups)
-        # offsets will be like [0, num_vars, 2*num_vars, ..., n*num_vars] where n = num_groups-1
-        offsets = torch.arange(0, self.num_vars * self.num_groups, self.num_vars)
-        index += offsets.unsqueeze(0)
-        # Extract rows from codevectors corresponding to indices in the index tensor
-        codevectors = torch.index_select(self.codevectors.squeeze(), 0, index.flatten())
-        codevectors = codevectors.view(batch_size, sequence_length, -1)
+        codevector_probs = codevector_probs.view(batch_size * sequence_length, self.num_groups, -1)
+        codebook = self.codevectors[0, :, :]
+        codebook = codebook.view(self.num_groups, self.num_vars, -1)
+        codevectors = torch.einsum('lgv,gvd->lgd', codevector_probs, codebook)
+
+        codevectors = codevectors.reshape(batch_size, sequence_length, -1)
 
         return codevectors, perplexity
