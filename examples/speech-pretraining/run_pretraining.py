Fix temperature annealing for gumbel softmax

examples/speech-pretraining/run_pretraining.py
@@ -110,6 +110,15 @@ class ModelArguments:
     ctc_loss_reduction: Optional[str] = field(
         default="mean", metadata={"help": "The way the ctc loss should be reduced. Should be one of 'mean' or 'sum'."}
     )
+    max_gumbel_temperature: Optional[float] = field(
+        default=2.0, metadata={"help": "Maximum temperature for gumbel softmax."}
+    )
+    min_gumbel_temperature: Optional[float] = field(
+        default=0.5, metadata={"help": "Minimum temperature for gumbel softmax."}
+    )
+    gumbel_temperature_decay: Optional[float] = field(
+        default=0.9, metadata={"help": "Decay of gumbel temperature during training."}
+    )
 
 
 @dataclass
@@ -218,12 +227,14 @@ class DataCollatorForWav2Vec2Pretraining:
             Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
         pad_to_multiple_of (:obj:`int`, `optional`):
             If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
     """
 
     model: Wav2Vec2ForPreTraining
     feature_extractor: Wav2Vec2FeatureExtractor
+    max_gumbel_temperature: float = 2.0
+    min_gumbel_temperature: float = 0.5
+    gumbel_temperature_decay: float = 0.9
+    global_step: int = 0
     padding: Union[bool, str] = "longest"
     pad_to_multiple_of: Optional[int] = None
 
@@ -272,6 +283,14 @@ class DataCollatorForWav2Vec2Pretraining:
         )
         batch["mask_time_indices"] = torch.tensor(mask_time_indices, dtype=torch.long, device=device)
         batch["sampled_negative_indices"] = torch.tensor(sampled_negative_indices, dtype=torch.long, device=device)
+        # Update the Gumbel temperature
+        gumbel_temperature = max(
+            self.max_gumbel_temperature * self.gumbel_temperature_decay**self.global_step,
+            self.min_gumbel_temperature,
+        )
+        self.model.set_gumbel_temperature(gumbel_temperature)
+        self.global_step += 1
+        batch["gumbel_temperature"] = torch.full([batch_size], gumbel_temperature, dtype=torch.float32)
         return batch.data
 
 
@@ -466,7 +485,10 @@ def main():
 
     # Instantiate custom data collator
     data_collator = DataCollatorForWav2Vec2Pretraining(
-        model=model, feature_extractor=feature_extractor)
+        model=model, feature_extractor=feature_extractor,
+        max_gumbel_temperature=model_args.max_gumbel_temperature,
+        min_gumbel_temperature=model_args.min_gumbel_temperature,
+        gumbel_temperature_decay=model_args.gumbel_temperature_decay)
 
     # Initialize Trainer
     trainer = IPUTrainer(

optimum/graphcore/models/wav2vec2/ipu_gumbel_vector_quantizer.py
@@ -47,29 +47,7 @@ def _ipu_gumbel_softmax(logits, tau=1, hard=False, eps=1e-10, dim=-1):
     
     
 class IPUWav2Vec2GumbelVectorQuantizer(Wav2Vec2GumbelVectorQuantizer):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        del self.temperature
-        self.register_buffer("temperature", torch.Tensor([2.0]))
-
-    def _get_temperature(self):
-        decay = 0.999995
-        end = 0.5
-
-        decayed_temp = self.temperature * decay
-        new_temp = torch.where(
-            decayed_temp >= torch.Tensor([end]),
-            decayed_temp, torch.Tensor([end])
-        )
-
-        self.temperature.copy_(new_temp)
-
-        return self.temperature
-
-
-    def forward(self, hidden_states, mask_time_indices=None):
+    def forward(self, hidden_states, gumbel_temperature, mask_time_indices=None):
         batch_size, sequence_length, hidden_size = hidden_states.shape
 
         # project to codevector dim
@@ -77,11 +55,9 @@ class IPUWav2Vec2GumbelVectorQuantizer(Wav2Vec2GumbelVectorQuantizer):
         hidden_states = hidden_states.view(batch_size * sequence_length * self.num_groups, -1)
 
         if self.training:
-            temperature = self._get_temperature()
-
             # sample code vector probs via gumbel in differentiateable way
             codevector_probs = _ipu_gumbel_softmax(
-                hidden_states.float(), tau=temperature, hard=True
+                hidden_states.float(), tau=gumbel_temperature, hard=True
             ).type_as(hidden_states)
 
             # compute perplexity